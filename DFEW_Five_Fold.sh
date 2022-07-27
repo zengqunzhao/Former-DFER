77bf5c4e3dc37957dd0e01a41090a835083c62d5
Zengqun Zhao <zqzhaocv@gmail.com>
2022-07-27 22:42:25 +0800

    Update DFEW_Five_Fold.sh

--- DFEW_Five_Fold.sh
+++ DFEW_Five_Fold.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-python main_DFEW.py --data_set 1 --gpu "0";
-python main_DFEW.py --data_set 2 --gpu "0";
-python main_DFEW.py --data_set 3 --gpu "0";
-python main_DFEW.py --data_set 4 --gpu "0";
-python main_DFEW.py --data_set 5 --gpu "0";
+CUDA_VISIBLE_DEVICES=0 python main_DFEW.py --data_set 1;
+CUDA_VISIBLE_DEVICES=0 python main_DFEW.py --data_set 2;
+CUDA_VISIBLE_DEVICES=0 python main_DFEW.py --data_set 3;
+CUDA_VISIBLE_DEVICES=0 python main_DFEW.py --data_set 4;
+CUDA_VISIBLE_DEVICES=0 python main_DFEW.py --data_set 5;
